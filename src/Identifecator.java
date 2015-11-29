7f9bbe347a261ad2651805096f8ff1921dcc3d93
VitaliyRakytyansky <Rakytyansky@rambler.ru>
2015-11-29 17:48:42 +0200

    Created student and ind.

---
+++ src/Identifecator.java
@@ -0,0 +1,21 @@
+
+public class Identifecator {
+
+		String key;
+
+		
+		
+		public Identifecator(String key) {
+			super();
+			this.key = key;
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+		
+}
